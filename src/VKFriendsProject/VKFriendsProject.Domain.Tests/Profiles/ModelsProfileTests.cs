2ec796ce5a6babadbc3d0ac46b62038da9d3559f
Sequenta <tranquilized.mind@gmail.com>
2015-02-22 15:09:47 +0500

    Тесты на профайл для маппинга

---
+++ src/VKFriendsProject/VKFriendsProject.Domain.Tests/Profiles/ModelsProfileTests.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using VKFriendsProject.Domain.Profiles;
+using Xunit;
+
+namespace VKFriendsProject.Domain.Tests.Profiles
+{
+    public class ModelsProfileTests
+    {
+        [Fact]
+        public void TestProfiles()
+        {
+            Mapper.AddProfile<ModelsProfile>();
+            Assert.DoesNotThrow(Mapper.AssertConfigurationIsValid); 
+        }
+    }
+}
